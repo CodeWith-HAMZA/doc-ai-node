353babba6fa4681fa391f3cf40b32fd14287363a
OnlineSourceS <115408642+CodeWith-HAMZA@users.noreply.github.com>
2024-10-20 12:13:05 +0500

    .

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  console.log(req.nextUrl);
+
+  const res = NextResponse.next();
+  res.headers.append("ACCESS-CONTROL-ALLOW-ORIGIN", "*");
+  return res;
+}
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
